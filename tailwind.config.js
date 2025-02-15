156b09cd440df0bd47d9482f1b523f412f292085
youseffathy <yf1102003@gmail.com>
2025-02-15 14:15:13 +0200

    feat: finish the signup page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ export default {
         dark: "#000",
         light: "#fff",
       },
+      screens: {
+        "xs": "500px", // Extra small screens
+        "xmd": "800px", // Extra small screens
+        "2xl": "1440px", // Extra large screens
+      },
     },
   },
   plugins: [],
