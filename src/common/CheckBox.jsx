a73686ad2db2e2bf64aa8f4cc30727827e2c4432
youseffathy <yf1102003@gmail.com>
2025-05-18 16:46:29 +0300

    feat: handle register page form validation

--- src/common/CheckBox.jsx
+++ src/common/CheckBox.jsx
@@ -1,13 +1,13 @@
 import "../global/styles.css";
 
-export default function CheckBox({ value, onChange, checked }) {
+export default function CheckBox({ value, onChange }) {
   return (
     <div className="checkmarkContainer">
       <input
         type="checkbox"
         value={value}
         onChange={onChange}
-        checked={checked ? true : false}
+        checked={value ? true : false}
       />
       <div className="checkmark"></div>
     </div>
