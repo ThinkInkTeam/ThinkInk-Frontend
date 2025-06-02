b344ee8201dfc558839a15450bba8d6d0eb7e2ae
youseffathy <yf1102003@gmail.com>
2025-06-02 19:08:38 +0300

    feat: implement pricing card component with green tag integration

--- src/common/SwitchBtn.jsx
+++ src/common/SwitchBtn.jsx
@@ -1,10 +1,10 @@
 import "../global/styles.css";
 
-export default function SwitchBtn({ checked, onChecked }) {
+export default function SwitchBtn({ checked, onChecked, ariaLabel }) {
   return (
-    <label class="switchBtn">
-      <input type="checkbox" value={checked} onChange={onChecked} />
-      <span class="slider"></span>
+    <label className="switchBtn" aria-label={ariaLabel}>
+      <input type="checkbox" value={checked} onChange={onChecked} id="Toggle-annual-billing"/>
+      <span className="slider"></span>
     </label>
   );
 }
